929478424f30e49eb2a5b81c17116ea783870282
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-21 12:52:32 +0700

    Create context, reducer and add action to action.js

---
+++ src/context.jsx
@@ -0,0 +1,9 @@
+import React, { createContext, useContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  return <AppContext.Provider>{children}</AppContext.Provider>;
+};
+
+export const useGlobalContext = () => useContext(AppContext);
