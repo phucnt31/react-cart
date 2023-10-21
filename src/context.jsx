b81249c4fd1d8b3b2aed0c93cfe6205a3b4effc3
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-21 12:58:32 +0700

    Setup context, action, reducer

--- src/context.jsx
+++ src/context.jsx
@@ -1,9 +1,27 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useReducer } from "react";
+import {
+  CLEAR_CART,
+  DECREASE,
+  DISPLAY_ITEMS,
+  INCREASE,
+  LOADING,
+  REMOVE_ITEM,
+} from "./action";
+import { reducer } from "./reducer";
 
 const AppContext = createContext();
 
+const defaultState = {
+  cart: new Map(),
+  isLoading: false,
+};
+
 export const AppProvider = ({ children }) => {
-  return <AppContext.Provider>{children}</AppContext.Provider>;
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => useContext(AppContext);
---
+++ src/reducer.js
@@ -0,0 +1,10 @@
+import {
+  CLEAR_CART,
+  DECREASE,
+  DISPLAY_ITEMS,
+  INCREASE,
+  LOADING,
+  REMOVE_ITEM,
+} from "./action";
+
+export const reducer = (state, action) => {};
